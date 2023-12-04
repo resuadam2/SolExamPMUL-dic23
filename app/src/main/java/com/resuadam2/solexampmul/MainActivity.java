14a78f3850e7da403f9bbea605b46b46d33fea3d
resuadam2 <resuadam2@gmail.com>
2023-12-04 21:54:31 +0100

    adding basic structure

--- app/src/main/java/com/resuadam2/solexampmul/MainActivity.java
+++ app/src/main/java/com/resuadam2/solexampmul/MainActivity.java
@@ -2,7 +2,12 @@ package com.resuadam2.solexampmul;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.resuadam2.solexampmul.ejer1.Ejer1Acivity;
+import com.resuadam2.solexampmul.ejer2.Ejer2Activity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnEjer1 = findViewById(R.id.btnEjer1);
+        Button btnEjer2 = findViewById(R.id.btnEjer2);
+
+        btnEjer1.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Ejer1Acivity.class);
+            startActivity(intent);
+        });
+
+        btnEjer2.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Ejer2Activity.class);
+            startActivity(intent);
+        });
     }
 }
---
+++ app/src/main/java/com/resuadam2/solexampmul/ejer1/Ejer1Acivity.java
@@ -0,0 +1,16 @@
+package com.resuadam2.solexampmul.ejer1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.resuadam2.solexampmul.R;
+
+public class Ejer1Acivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejer1);
+    }
+}
